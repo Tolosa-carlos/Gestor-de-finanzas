058304d01f497f78eea17a8188a898b9d796e785
Carlos Tolosa <carloswtolosa@gmail.com>
2026-07-29 12:12:20 -0300

    Formularios, navegacion y diseño hechos

--- finanzas_flask/app.py
+++ finanzas_flask/app.py
@@ -1,17 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from database import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hola desde Flask!'
+    ingresos, gastos = calcular_balance()
+    return render_template('index.html', ingresos = ingresos, gastos = gastos)
 
 
 @app.route('/transacciones')
 def transacciones():
     datos = obtener_transacciones()
     return render_template('transacciones.html', transacciones = datos)
+
+@app.route('/registrar', methods=['GET', 'POST'])
+def registrar():
+    if request.method == 'POST':
+        descripcion = request.form['descripcion']
+        monto = float(request.form['monto'])
+        fecha = request.form['fecha']
+        tipo = request.form['tipo']
+        categoria_id = int(request.form['categoria_id'])
+        registrar_transaccion(descripcion, monto, fecha, tipo, categoria_id)
+        return redirect(url_for('transacciones'))
+    
+    categorias = obtener_categorias()
+    return render_template('registrar.html', categorias = categorias)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
